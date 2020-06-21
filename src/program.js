dc6112353d32642e908f724b324fc9021763f97e
AntoineC <antoine@a-f.studio>
2020-06-21 18:01:20 +0200

    wip (tcr)

--- src/program.js
+++ src/program.js
@@ -43,8 +43,8 @@ export default function statement(invoices, plays) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
     //print line for this order
-    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`;
