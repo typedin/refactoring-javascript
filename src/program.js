a1eb7837ca6fe59f3eca677c58f69a26ee9112a8
AntoineC <antoine@a-f.studio>
2020-06-21 18:35:30 +0200

    Lots of nested functions
    wip (tcr)
    
    wip (tcr)

--- src/program.js
+++ src/program.js
@@ -9,6 +9,30 @@ export default function statement(invoices, plays) {
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
+  function totalAmount(){
+    let result = 0;
+    for (let perf of invoices.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoices.performances) {
+      result  += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber / 100)
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -32,13 +56,11 @@ export default function statement(invoices, plays) {
       default:
         throw new Error("unknown type: ${playFor(aPerformance).type}");
     }
-    
     return result
   }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0
-
     result += Math.max(aPerformance.audience - 30, 0);
     if ("comedy" === playFor(aPerformance).type) {
       result += Math.floor(aPerformance.audience / 5);
@@ -46,28 +68,5 @@ export default function statement(invoices, plays) {
     return result;
   }
   
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100)
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoices.performances) {
-      result  += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function totalAmount(){
-    let result = 0;
-    for (let perf of invoices.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
 }
 
