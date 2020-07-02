3279dd239b917f822a3c7db85ef79e42dad132f1
AntoineC <antoine@a-f.studio>
2020-07-02 15:32:43 +0200

    wip (tcr)

--- src/program.js
+++ src/program.js
@@ -7,11 +7,12 @@ export default function statement(invoices, plays) {
   return renderPlainText(statementData, plays);
 
   function totalAmount(data){
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    //let result = 0;
+    //for (let perf of data.performances) {
+      //result += perf.amount;
+    //}
+    //return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
